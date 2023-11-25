eabeeb59b725af4348861a5c2ae5b2802fc6d272
salgadoIbacache <se.salgado@duocuc.cl>
2023-11-25 16:13:29 -0300

    Se agre a push.services notificacionRecibida

--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { OneSignal } from '@ionic-native/onesignal/ngx';
+
+import { OneSignal , OSNotification } from '@ionic-native/onesignal/ngx';
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +19,24 @@ export class PushService {
 
 configuracionInicial() {
   this.oneSignal.startInit('', '');
-  this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+  this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
   this.oneSignal.handleNotificationReceived().subscribe(( noti ) => {
     console.log('Notificacion recibida', noti);
+    this.notificacionRecibida( noti );
   });
   this.oneSignal.handleNotificationOpened().subscribe(( noti ) => {
     console.log('Notificación abierta')
   });
 }
+  notificacionRecibida( noti: OSNotification)  {
+    const payload = noti.payload;
+    const existePush = this.mensajes.find( mensaje => mensaje.notificationID ===
+      payload?.notificationID);
+
+      if( existePush ) {
+        return
+      }
+
+      this.mensajes.unshift( payload );
+  }
 }
